6a98860846ad263ad7aa91ff2981c1cd4c83f3c7
samsaji01 <59938056+samsaji01@users.noreply.github.com>
2021-02-15 18:12:47 -0500

    did a lot of stuff from class

---
+++ practicemvc/Controllers/StoreController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using practicemvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace practicemvc.Controllers
+{
+    public class StoreController : Controller
+    {
+        public IActionResult Index()
+        {
+            // use faek category class/model to create and display 10 categories
+            //1. cretate an object to hold  a list of categories
+            var categories = new List<Category>();
+            for(var i=1; i<=10; i++)
+            {
+                categories.Add(new Category { Id = i, Name = "Category " + i.ToString() });
+            }
+            // modify  the return view so that it now accepts a list of categories to pass to the view of the display
+
+            return View(categories);
+        }
+
+        public IActionResult Browse(string category)
+        {
+            ViewBag.category = category;
+            return View();
+        }
+    }
+}
